147ec4bf6d2707beb24b7940db7d172988db6a50
Tyler Wilhelm <tlwilhelm91@gmail.com>
2019-10-14 10:50:53 -0600

    boilerplate upload

---
+++ client-react/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Project from "./components/Project";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Project />
+    </div>
+  );
+}
+
+export default App;
